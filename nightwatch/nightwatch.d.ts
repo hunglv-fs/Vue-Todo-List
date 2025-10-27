43bcbc55f3df407316bbc40487296e9e61bfbc86
HungLV_FullStack <hunglv.tech@gmail.com>
2025-10-27 11:31:29 +0700

    fix(lint) Remove variable unused

--- nightwatch/nightwatch.d.ts
+++ nightwatch/nightwatch.d.ts
@@ -1,11 +1,11 @@
-import { NightwatchCustomAssertions, NightwatchCustomCommands } from 'nightwatch'
-
 declare module 'nightwatch' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface NightwatchCustomAssertions {
     // Add your custom assertions' types here
     // elementHasCount: (selector: string, count: number) => NightwatchBrowser
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface NightwatchCustomCommands {
     // Add your custom commands' types here
     // strictClick: (selector: string) => NightwatchBrowser
